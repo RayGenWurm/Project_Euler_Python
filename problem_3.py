3d1d72dc7e0a2997e91ac2ff2d6d9b5805577c0b
RayGenWurm <github@gratzl-design.at>
2024-11-02 00:04:14 +0100

    solution for problem 3

---
+++ problem_3.py
@@ -0,0 +1,26 @@
+# https://projecteuler.net/problem=3
+# "Largest Prime Factor"
+# Nov-2024 / RayGenWurm
+
+# first try - simple brute forcing
+
+import math
+
+given_number = 600851475143
+# given_number = 13195
+
+def is_prime(number):
+    state = True
+    for x in range(2, (int(number/2))):
+        if number % x == 0:
+            state = False
+            break
+    return state
+
+
+for i in range(int(math.sqrt(given_number) / 2), 0, -2):
+    if given_number % i == 0:
+        if is_prime(i):
+            print(str(i))
+            break
+
