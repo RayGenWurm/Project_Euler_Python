7beb772a4778b28077faab1978ed57f5b5dd5ea3
RayGenWurm <github@gratzl-design.at>
2024-11-01 22:24:19 +0100

    solution for first problem

---
+++ problem_1.py
@@ -0,0 +1,11 @@
+# https://projecteuler.net/problem=1
+# "Multiples of 3 or 5"
+
+limit = 1000
+calc_sum = 0
+
+for i in range(limit):
+    if i % 3 == 0 or i % 5 == 0:
+        calc_sum += i
+
+print(str(calc_sum))
