73c80b848b7fc5fc1bf690f70cc06ff192c18a37
RayGenWurm <github@gratzl-design.at>
2024-11-06 18:29:25 +0100

    waaaay better solution for problem 10

--- problem_10_v2.py
+++ problem_10_v2.py
@@ -3,9 +3,12 @@
 # Nov-2024 / RayGenWurm
 
 # plain brute force is taking way too long...
-# in this first try i am using the "Sieve of Eratosthenes" method
+# in this second try I am using optimized the "Sieve of Eratosthenes" method
 # https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
-# took me about 50 MINUTES to compute... but it works!
+# in insight... last attempt was just stupid...
+# took only 0.5 seconds - im fine with that
+
+# I guess deleting non-prime numbers would further reduce run time but that's changing the list length and im too lazy to counter that
 
 import time, math
 start = time.time()
@@ -15,29 +18,28 @@ check_stop = int(math.sqrt(limit))
 prime_sum = 0
 prime_list = [2]
 
-# fill list with all uneven candidates
+# fill a list with all uneven candidates
 for i in range(3, limit, 2):
     prime_list.append(i)
 
-# perform Sieve of Eratosthenes algo
+# perform Sieve of Eratosthenes algo - 0 all non-prime numbers
 check_pos = 1
 while prime_list[check_pos] <= check_stop:
     divider = prime_list[check_pos]
-    print(divider)
-    for index, item in enumerate(list(prime_list)):
-        if index == check_pos:
-            continue
-        if index % 1000 == 0:
-            print(divider, index)
-        if item % divider == 0:
-            prime_list.remove(item)
+    if divider != 0:
+        for z in range(check_pos, int(limit/2), prime_list[check_pos]):
+            prime_list[z] = 0
+    prime_list[check_pos] = divider
     check_pos += 1
+
+
 
 # now our list only contains prime numbers til set limit
 # now just sum them up
 
 for x in prime_list:
-    prime_sum += x
+    if x != 0:
+        prime_sum += x
 
 print()
 print("final sum:", prime_sum)
