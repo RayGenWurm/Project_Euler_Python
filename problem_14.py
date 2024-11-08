82963a32e941839283c65586fd3c2157a2bc4e27
RayGenWurm <github@gratzl-design.at>
2024-11-08 21:05:42 +0100

    solution for problem 14

---
+++ problem_14.py
@@ -0,0 +1,37 @@
+# https://projecteuler.net/problem=14
+# "Longest Collatz Sequence"
+# Nov-2024 / RayGenWurm
+
+# runs in about 14 seconds... I have no idea how to improve this so i will go with it
+
+import time, math
+start = time.time()
+
+
+limit = 1000000
+highest = 0
+highest_i = 0
+
+def calc_collatz(n):
+    counter = 1
+    while n > 1:
+        if n % 2 == 0:
+            n /= 2
+        else:
+            n = 3 * n + 1
+        counter += 1
+    return counter
+
+for i in range(1, limit, 2):
+    if ((i - 1) // 2 + 1) % 1000 == 0: # just to print some progress while waiting
+        print("progress:",i, "of 1000000")
+    tmp = calc_collatz(i)
+    if tmp > highest:
+        highest = tmp
+        highest_i = i
+
+print(highest, highest_i)
+
+end = time.time()
+print()
+print(end - start, "seconds")
